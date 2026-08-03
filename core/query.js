e27503b817c95269c379e194453160e6ff33bd6e
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-03 09:50:41 +0200

    Update query.js

--- core/query.js
+++ core/query.js
@@ -4,9 +4,28 @@ import { _doc, _slct, _el } from './internal/resolve.js';
 
 export const
 
-getElement     = (spec, ctx) => _doc(ctx).querySelector(_slct(spec)) ?? null,
-getElements    = (spec, ctx) => [..._doc(ctx).querySelectorAll(_slct(spec))],
-getElementById = (id, ctx) => _doc(ctx).getElementById?.(id) ?? getElement(`#${id}`, ctx),
+/*
+document.getElementsByClassName()
+document.getElementsByTagName()
+
+document.anchors
+document.body
+document.documentElement
+document.embeds
+document.forms
+document.head
+document.images
+document.links
+document.scripts
+document.title
+
+*/
+
+getElement         = (spec, ctx) => _doc(ctx).querySelector(_slct(spec)) ?? null,
+getElements        = (spec, ctx) => [..._doc(ctx).querySelectorAll(_slct(spec))],
+getElementById     = (id,   ctx) => _doc(ctx).getElementById?.(id) ?? getElement(`#${id}`, ctx),
+getElementsByClass = (name, ctx) => [..._doc(ctx).getElementsByClassName (name)],
+getElementsByTag   = (name, ctx) => [..._doc(ctx).getElementsByTagName   (name)],
 
 getElementsByDataAttr = (key, ctx) => getElements(`[data-${key}]`, ctx),
 getElementsByDataKey  = (key, ctx) => getElements(`[data-key="${key}"]`, ctx),
