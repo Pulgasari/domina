0b28a858377c0d7218e7c1371284d96b1b289768
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-03 07:29:50 +0200

    Create el.js

---
+++ el.js
@@ -0,0 +1,4 @@
+// el.js - the chain
+// domina ist not intended to be a chain like jQuery
+// however: there is nothing wrong about offering a chain additionally
+// so the plan is to construct a `el` here by making use of the lib stuff
