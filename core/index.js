7d7310f3a18f44fcd6f0edec761eb578759877b3
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-03 12:00:52 +0200

    Update index.js

--- core/index.js
+++ core/index.js
@@ -1,5 +1,19 @@
+// @ts-self-types="./mod.d.ts"
 // @domina/core
 
-export { filterElements } from './filter.js';
-export { groupElements }  from './group.js';
-export { sortElements }   from './sort.js';
+export * from './create.js';
+export * from './events.js';
+export * from './form.js';
+export * from './misc.js';
+export * from './observer.js';
+export * from './query.js';
+export * from './raf.js';
+export * from './update.js';
+export * from './values.js';
+export * from './collection/index.js';
+
+// Aliase
+export { 
+  element  : getElement, 
+  elements : getElements,
+} from './src/query.js';
