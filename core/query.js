76305c88a71c1e7bd630ffc97d7e1b4dea62115b
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-03 21:39:54 +0200

    Update query.js

--- core/query.js
+++ core/query.js
@@ -4,26 +4,6 @@ import { _doc, _slct, _el } from './internal/resolve.js';
 
 export const
 
-/*
-document.getElementsByClassName()
-document.getElementsByTagName()
-
-document.anchors
-document.body
-document.documentElement
-document.embeds
-document.forms
-document.head
-document.images
-document.links
-document.scripts
-document.title
-
-.insertAdjacentText()
-.insertAdjacentElement()
-
-*/
-
 getElement         = (spec, ctx) => _doc(ctx).querySelector(_slct(spec)) ?? null,
 getElements        = (spec, ctx) => [..._doc(ctx).querySelectorAll(_slct(spec))],
 getElementById     = (id,   ctx) => _doc(ctx).getElementById?.(id) ?? getElement(`#${id}`, ctx),
