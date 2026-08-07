878a5e389d04960caa7e594f83cebe2a98efe58b
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-07 09:01:36 +0200

    Update element.js

--- core/element.js
+++ core/element.js
@@ -8,17 +8,14 @@ import { getAttr, hasAttr, removeAttr, setAttr, toggleAttr } from './attr.js';
 import { emitEvent, offEvent, onEvent, onceEvent, delegate } from './events.js';
 
 export const
-
 getElement         = (spec, ctx) => _doc(ctx).querySelector(_slct(spec)) ?? null,
 getElements        = (spec, ctx) => [..._doc(ctx).querySelectorAll(_slct(spec))],
 getElementById     = (id,   ctx) => _doc(ctx).getElementById?.(id) ?? getElement(`#${id}`, ctx),
 getElementsByClass = (name, ctx) => { const d = _doc(ctx); return d.getElementsByClassName ? [...d.getElementsByClassName(name)] : getElements(`.${name}`, ctx); },    
 getElementsByName  = (name, ctx) => getElements(`[name="${name}"]`, ctx),
 getElementsByTag   = (name, ctx) => [..._doc(ctx).getElementsByTagName   (name)],
-
 getElementsByDataAttr = (key, ctx) => getElements(`[data-${key}]`, ctx),
 getElementsByDataKey  = (key, ctx) => getElements(`[data-key="${key}"]`, ctx),
-
 clone = (spec, deep = true) => _el(spec)?.cloneNode(deep) ?? null;
 
 export const updateElement = (spec, props = {}, ...children) => {
@@ -76,7 +73,7 @@ export const updateElement = (spec, props = {}, ...children) => {
 
 
 
-export createElement = (tag = 'div', props = {}, ...children) =>
+export const createElement = (tag = 'div', props = {}, ...children) =>
   updateElement(document.createElement(tag), props, ...children),
 
 // ::::::
