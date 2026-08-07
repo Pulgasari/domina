41ad17b3dcd82e5967fdf7949ea8d8e98a686a68
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-07 08:47:42 +0200

    Update index.js

--- core/index.js
+++ core/index.js
@@ -9,7 +9,7 @@ export * from './events.js';
 export * from './form.js';
 export * from './misc.js';
 export * from './observer.js';
-export * from './query.js';
+//export * from './query.js';
 export * from './raf.js';
 export * from './update.js';
 export * from './values.js';
