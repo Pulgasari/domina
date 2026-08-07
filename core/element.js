dd5634ffecb1258d614662a9de59689a16160cbf
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-07 08:51:54 +0200

    Update element.js

--- core/element.js
+++ core/element.js
@@ -1,7 +1,7 @@
 // @domina/core/element.js
 
 import { _doc, _slct, _el }     from './internal/resolve.js';
-import { isArray }              from './internal/is.js';
+import { isArray, isFn }        from './internal/is.js';
 import { getValue, setValue }   from './values.js';
 import { moveTo, unwrap, wrap } from './misc.js';
 import * from './attr.js';
