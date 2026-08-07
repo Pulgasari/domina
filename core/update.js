b5d2a5143c7da1aa3bbede58a0ede823beeeb0d7
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-07 08:47:07 +0200

    Update update.js

--- core/update.js
+++ core/update.js
@@ -1,6 +1,7 @@
 // update.js
 
 import { _el } from './internal/resolve.js';
+import { updateElement } from './element.js';
 import { isArray, isFn, isString } from './internal/is.js';
 import { flatNodes } from './internal/normalize.js';
 import { onAdded, onAttr, onConnected, onDisconnected, onRemoved, onResize, onVisible } from './observer.js';
@@ -9,57 +10,7 @@ const observerEvents = { onAdded, onAttr, onConnected, onDisconnected, onRemoved
 
 // todo: updateHead, updateTitle, updateMeta, updateStylesheet
 
-export const updateElement = (spec, props = {}, ...children) => {
-  const element = _el(spec);
-  if (!element) return null;
 
-  // SVG-Elemente haben read-only Props (className, href) -> immer setAttribute
-  const isSVG = element instanceof SVGElement;
-  let mountFn, mountTo;
-
-  for (const [key, value] of Object.entries(props)) {
-    if (value == null) continue;
-
-    // appendTo + prependTo
-    if      (key === 'appendTo')  { mountTo = value; mountFn = 'append';  }
-    else if (key === 'prependTo') { mountTo = value; mountFn = 'prepend'; }
-
-    // style
-    else if (key === 'style') {
-      if (isString(value)) element.setAttribute('style', value);
-      else for (const [p, v] of Object.entries(value))
-        p.includes('-') ? element.style.setProperty(p, v) : (element.style[p] = v);
-    }
-
-    // dataset
-    else if (key === 'dataset' || key === 'data') {
-      Object.assign(element.dataset, value);
-    }
-
-    // class
-    else if (key === 'class' || key === 'className') {
-      element.setAttribute('class',
-        isArray(value) ? value.flat(Infinity).filter(Boolean).join(' ') : value);
-    }
-
-    // event / observer
-    else if (key.startsWith('on') && isFn(value)) {
-      const observerFn = observerEvents[key];
-      observerFn ? observerFn(element, value)
-                 : element.addEventListener(key.slice(2).toLowerCase(), value);
-    }
-
-    // prop + attribute
-    else if (!isSVG && key in element) element[key] = value;
-    else element.setAttribute(key, value);
-  }
-
-  const kids = flatNodes(children);
-  if (kids.length) element.append(...kids);
-  if (mountTo) _el(mountTo)?.[mountFn](element);
-
-  return element;
-};
 
 // Findet oder erzeugt ein Element im <head>, identifiziert über einen Selektor.
 const upsertHead = (selector, make) => {
