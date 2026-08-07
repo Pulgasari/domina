6edb1e61b6bdf7e6bda721ba7dbe95b06dada54f
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-07 09:16:15 +0200

    Update events.js

--- core/events.js
+++ core/events.js
@@ -3,7 +3,7 @@
 import { _el, _tgt } from './internal/resolve.js';
 import { isArray, isFn, isString } from './internal/is.js';
 import { arrayfied } from './internal/normalize.js';
-import { getElements } from './query.js';
+import { getElements } from './element.js';
 
 const isIterable = v => !isString(v) && isFn(v?.[Symbol.iterator]);
 
