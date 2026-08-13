887f406742607d37736bcb2a0c22bb3ca9ec0e51
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-13 10:12:29 +0200

    Update createStyleElement.js

--- core/methods/createStyleElement.js
+++ core/methods/createStyleElement.js
@@ -3,7 +3,9 @@
 import { isString }  from './../internal/is.js';
 import createElement from './createElement.js';
 
-export default const createStyleElement = source => {
+export const createStyleElement = source => {
   const body = isString(source) ? { textContent: source } : source ?? {};
   return createElement('style', body);
-}
+};
+
+export default createStyleElement;
