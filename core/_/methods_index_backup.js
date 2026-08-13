97d5ffa216e6b1962b9aa0dc29c68d8334d8a205
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-13 17:49:38 +0200

    Create methods_index_backup.js

---
+++ core/_/methods_index_backup.js
@@ -0,0 +1,103 @@
+// @domina/core/methods/index.js
+
+export * from './addClass.js';
+export * from './addFont.js';
+export * from './adoptStylesheet.js';
+export * from './appendToElement.js';
+
+export * from './createStylesheet.js';
+
+export * from './delegateEvent.js';
+
+export * from './eachFont.js';
+export * from './emitEvent.js';
+export * from './emptyElement.js';
+
+export * from './fontsReady.js';
+
+export * from './getAttr.js';
+export * from './getClass.js';
+export * from './getCssVar.js';
+export * from './getData.js';
+export * from './getElementOffset.js';
+export * from './getElementOffsetToParent.js';
+export * from './getElementPosition.js';
+export * from './getElementRect.js';
+export * from './getElementSize.js';
+export * from './getFontStatus.js';
+export * from './getFonts.js';
+export * from './getFormValues.js';
+export * from './getHTML.js';
+export * from './getMeta.js';
+export * from './getMetaAttr.js';
+export * from './getMetaElement.js';
+export * from './getScroll.js';
+export * from './getStyle.js';
+export * from './getStylesheets.js';
+export * from './getText.js';
+export * from './getValue.js';
+
+export * from './hasAttr.js';
+export * from './hasClass.js';
+export * from './hasData.js';
+export * from './hasFont.js';
+export * from './hasMeta.js';
+export * from './hasStylesheet.js';
+
+export * from './insertAfter.js';
+export * from './insertAt.js';
+export * from './insertBefore.js';
+export * from './isElementInViewport.js';
+
+export * from './jumpTo.js';
+export * from './jumpToTop.js';
+
+export * from './loadFont.js';
+
+export * from './moveTo.js';
+
+export * from './notifyChange.js';
+
+export * from './offEvent.js';
+export * from './onCustom.js';
+export * from './onEvent.js';
+export * from './onOutside.js';
+export * from './onceEvent.js';
+
+export * from './prependToElement.js';
+
+export * from './releaseStylesheet.js';
+export * from './removeAttr.js';
+export * from './removeClass.js';
+export * from './removeData.js';
+export * from './removeElement.js';
+export * from './removeFont.js';
+export * from './removeMeta.js';
+export * from './removeStyle.js';
+export * from './replaceClass.js';
+export * from './replaceElement.js';
+
+export * from './scopeStylesheet.js';
+export * from './scrollTo.js';
+export * from './scrollToTop.js';
+export * from './setAttr.js';
+export * from './setClass.js';
+export * from './setContent.js';
+export * from './setCssVar.js';
+export * from './setData.js';
+export * from './setFormValues.js';
+export * from './setHTML.js';
+export * from './setMeta.js';
+export * from './setScroll.js';
+export * from './setStyle.js';
+export * from './setText.js';
+export * from './setValue.js';
+
+export * from './toggleAttr.js';
+export * from './toggleClass.js';
+
+export * from './unwrapElement.js';
+export * from './updateMeta.js';
+
+export * from './waitForEvent.js';
+export * from './wrapElement.js';
