df445d5f37a017613ab9158065cb7d1ff0b01688
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-13 09:53:23 +0200

    Create createSVG.js

---
+++ core/methods/createSVG.js
@@ -0,0 +1,14 @@
+// createSVG.js
+
+import updateElement from './updateElement.js';
+
+const SVG_NS = 'http://www.w3.org/2000/svg';
+
+export default const createSVG = (
+  tag = 'svg', 
+  props = {}, 
+  ...children
+) => {
+  const element = document.createElementNS(SVG_NS, tag);
+  return updateElement(element, props, ...children);
+}
