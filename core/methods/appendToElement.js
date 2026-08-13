d590fad7ba97eea62a6f14114503757296136399
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-13 16:39:00 +0200

    Create appendToElement.js

---
+++ core/methods/appendToElement.js
@@ -0,0 +1,7 @@
+// appendToElement.js
+
+import moveToElement from './moveToElement.js';
+
+export const appendToElement = (spec, target) => moveToElement (spec, target, 'append');
+
+export default appendToElement;
