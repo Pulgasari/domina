bf5dc11d2c6c563886e4ffeb929a07e9e9caa1d2
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-13 11:26:04 +0200

    Update addClass.js

--- core/methods/addClass.js
+++ core/methods/addClass.js
@@ -4,9 +4,9 @@ import { _el }    from './../resolve.js';
 import { toList } from './../utils.js';
 
 export const addClass = (spec, ...names) => {
-  const element = _el(spec);
-  if (!element) return null;
-  const list = toList(names);
+  const element = _el(spec); if (!element) return null;
+  const list    = toList(names);
+  
   if (list.length) element.classList.add(...list);
   return element;
 };
