ef80c9571388daca628915288a3720bf576d5c0a
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-14 01:57:19 +0200

    Refactor import statements in setHead.js

---
+++ core/methods/setHead.js
@@ -0,0 +1,26 @@
+// setHead.js
+
+import { arrayfied, isObject } from './../vendors.js';
+import { setTitle }      from './setTitle.js';
+import { setMeta }       from './setMeta.js';
+import { setLink }       from './setLink.js';
+import { updateElement } from './updateElement.js';
+
+/**
+ * setHead({ title, meta, link, ...props })
+ * meta takes a key/value map (or an array of them), link descriptor objects.
+ * Remaining props are applied directly to <head> via updateElement.
+ */
+export const setHead = ({ title, meta, link, ...props } = {}) => {
+  const head = document.head;
+  if (!head) return null;
+
+  if (title != null) setTitle(title);
+  for (const spec of arrayfied(meta)) if (isObject(spec)) setMeta(spec);
+  for (const spec of arrayfied(link)) setLink(spec);
+  if (Object.keys(props).length) updateElement(head, props);
+
+  return head;
+};
+
+export default setHead;
