e41632381b6f9df1f08aea08596f35db9f0d1301
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-13 10:11:30 +0200

    Update createHTML.js

--- core/methods/createHTML.js
+++ core/methods/createHTML.js
@@ -5,8 +5,10 @@
 // -> DocumentFragment. Der Umweg über <template> parst auch <tr> und <option>
 // korrekt, die in einem beliebigen Container still verworfen würden.
 
-export default const createHTML = html => {
+export const createHTML = html => {
   const template = document.createElement('template');
   template.innerHTML = String(html).trim();
   return template.content;
-}
+};
+
+export default createHTML;
