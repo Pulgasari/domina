cd4d869035d4fc6a11945e9386d8d8c8f613b23d
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-13 14:39:59 +0200

    Create removeFont.js

---
+++ core/methods/removeFont.js
@@ -0,0 +1,18 @@
+// removeFont.js
+
+import { getFonts } from './getFonts.js';
+
+const fontSet = () => (typeof document !== 'undefined' ? document.fonts : null);
+
+export const removeFont = (...families) => {
+  const fonts = fontSet();
+  if (!fonts) return [];
+
+  const removed = [];
+  for (const family of families.flat(Infinity)) {
+    for (const face of getFonts(family)) { fonts.delete(face); removed.push(face); }
+  }
+  return removed;
+};
+
+export default removeFont;
