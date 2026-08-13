64a6b0802690053e4a94a6656cb89b29298b7737
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-13 11:23:50 +0200

    Create hasClass.js

---
+++ core/methods/hasClass.js
@@ -0,0 +1,13 @@
+// @domina/core/methods/hasClass.js
+
+import { _el }    from './../resolve.js';
+import { toList } from './../vendors.js';
+
+export const hasClass = (spec, names) => {
+  const element = _el(spec); if (!element) return false;
+  const list    = toList(names);
+  
+  return list.length > 0 && list.every(name => element.classList.contains(name));   
+};
+
+export default hasClass;
