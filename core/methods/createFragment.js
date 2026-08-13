44f208d70969dbf89567d8ab27027fb872f2b89f
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-13 09:50:07 +0200

    Create createFragment.js

---
+++ core/methods/createFragment.js
@@ -0,0 +1,11 @@
+// createFragment.js
+
+import { flatNodes } from './../internal/normalize.js';
+
+export default const createFragment = (...nodes) => {
+  const fragment = document.createDocumentFragment();
+  const children = flatNodes(nodes);
+  
+  if (children.length) fragment.append(...children);
+  return fragment;
+}
