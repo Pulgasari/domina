1df5486e6783c2753a45c62e61068d324e26b951
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-13 11:17:41 +0200

    Create getMetaAttr.js

---
+++ core/methods/getMetaAttr.js
@@ -0,0 +1,23 @@
+// @domina/core/methods/getMetaAttr.js
+
+import { isString } from './../vendors.js';
+
+const HTTP_EQUIV_KEYS = new Set([
+  'content-type',
+  'default-style',
+  'refresh',
+  'x-ua-compatible',
+  'content-security-policy',
+]);
+
+// Das Attribut ergibt sich aus dem Key:
+// OpenGraph/Twitter/eigene Namespaces (mit ':')
+// -> property, HTTP-Header -> http-equiv, sonst name
+
+export const getMetaAttr = key =>
+    !isString(key)                          ? 'name'
+  : HTTP_EQUIV_KEYS.has(key.toLowerCase())  ? 'http-equiv'
+  : key.includes(':')                       ? 'property'
+  : 'name';
+
+export default getMetaAttr;
