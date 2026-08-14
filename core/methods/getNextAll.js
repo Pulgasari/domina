9509600ac0ecbb5fee41cc0ca45c99645a0f5739
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-14 12:47:34 +0200

    Update getNextAll.js

--- core/methods/getNextAll.js
+++ core/methods/getNextAll.js
@@ -1,22 +1,9 @@
 // @domina/core/methods/getNextAll.js
 
 import { _el, _slct } from './../resolve.js';
+import { walk }       from './../utils.js';
 
 const passes = (element, filter) => !filter || element.matches(_slct(filter));
-
-const walk = (element, direction, filter, all) => {
-  const found = [];
-  let current = element?.[direction];
-
-  while (current) {
-    if (passes(current, filter)) {
-      found.push(current);
-      if (!all) break;
-    }
-    current = current[direction];
-  }
-  return found;
-};
 
 export const   getNextAll = (spec, filter) => walk(_el(spec), 'nextElementSibling', filter, true);   
 export default getNextAll;
