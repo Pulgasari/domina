dbf98f4caa4dfd8f4bdad7c44e0d33f951678922
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-14 16:03:08 +0200

    Update getSiblings.js

--- core/methods/getSiblings.js
+++ core/methods/getSiblings.js
@@ -1,14 +1,16 @@
 // getSiblings.js
 
-import { resolveElement } from './resolveElement.js';
 import { buildSelector }  from './buildSelector.js';
+import { resolveElement } from './resolveElement.js';
 
 const passes = (element, filter) => !filter || element.matches(buildSelector(filter));
 
 export function getSiblings (spec, filter) {
   const element = resolveElement(spec);
   if (!element?.parentElement) return [];
-  return [...element.parentElement.children].filter(child => child !== element && passes(child, filter));
+  return [...element.parentElement.children].filter(
+    child => child !== element && passes(child, filter)
+  );
 }
 
 export default getSiblings;
