08932b43a6052196ef9b1f49f40e8161e43d8eec
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-14 16:00:32 +0200

    Update getNext.js

--- core/methods/getNext.js
+++ core/methods/getNext.js
@@ -1,8 +1,11 @@
 // getNext.js
 
-import { resolveElement } from './resolveElement.js';
 import { buildSelector }  from './buildSelector.js';
+import { resolveElement } from './resolveElement.js';
 import { walk }           from './../utils.js';
 
-export function getNext (spec, filter) { return walk(resolveElement(spec), 'nextElementSibling', filter, false)[0] ?? null; }
-export default  getNext;
+export function getNext (spec, filter) {
+  return walk (resolveElement(spec), 'nextElementSibling', filter, false)[0] ?? null;
+}
+
+export default getNext;
