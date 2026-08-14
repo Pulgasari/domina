19c9b3cd3fbd1061c0f6d5fe6a3293bd596461bb
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-14 13:31:53 +0200

    Update clearElement.js

--- core/methods/clearElement.js
+++ core/methods/clearElement.js
@@ -2,6 +2,5 @@
 
 import { _el } from './../resolve.js';
 
-export const clearElement = spec => _el(spec)?.replaceChildren() ?? null;
-
+export const   clearElement = spec => _el(spec)?.replaceChildren() ?? null;
 export default clearElement;
