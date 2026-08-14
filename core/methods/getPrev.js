a1667f1530cc3e2f8d7ac1463115540fd200f070
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-14 16:03:53 +0200

    Update getPrev.js

--- core/methods/getPrev.js
+++ core/methods/getPrev.js
@@ -1,10 +1,11 @@
 // getPrev.js
 
-import { resolveElement } from './resolveElement.js';
 import { buildSelector }  from './buildSelector.js';
+import { resolveElement } from './resolveElement.js';
 import { walk }           from './../utils.js';
 
-const passes = (element, filter) => !filter || element.matches(buildSelector(filter));
+export function getPrev (spec, filter) {
+  return walk (resolveElement(spec), 'previousElementSibling', filter, false)[0] ?? null;
+}
 
-export function getPrev (spec, filter) { return walk(resolveElement(spec), 'previousElementSibling', filter, false)[0] ?? null; }
 export default getPrev;
