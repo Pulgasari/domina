e93c29f8c2a07773cae2fa88839ba943c818b8e8
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-14 12:46:36 +0200

    Update getPrevAll.js

--- core/methods/getPrevAll.js
+++ core/methods/getPrevAll.js
@@ -1,22 +1,9 @@
 // getPrevAll.js
 
 import { _el, _slct } from './../resolve.js';
+import { walk }       from './../utils.js';
 
 const passes = (element, filter) => !filter || element.matches(_slct(filter));
-
-const walk = (element, direction, filter, all) => {
-  const found = [];
-  let current = element?.[direction];
-
-  while (current) {
-    if (passes(current, filter)) {
-      found.push(current);
-      if (!all) break;
-    }
-    current = current[direction];
-  }
-  return found;
-};
 
 export const   getPrevAll = (spec, filter) => walk(_el(spec), 'previousElementSibling', filter, true);   
 export default getPrevAll;
