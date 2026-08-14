253b6080bf7af35688ff4f0b66a8eb78156e0958
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-14 15:46:40 +0200

    Add arrayfied and shuffle utility functions
    Added utility functions for array manipulation.

--- core/vendors.js
+++ core/vendors.js
@@ -3,3 +3,14 @@
 export * from 'https://code.pulgasari.dev/js/is.js';
 export * from 'https://code.pulgasari.dev/js/logger.js';
 export * from 'https://code.pulgasari.dev/js/str.js';
+
+export const
+arrayfied = v => isNullish(v) ? [] : isArray(v) ? v : [v],
+shuffle = arr => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+};
+
