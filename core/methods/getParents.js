1a4fc47fc3232defc34a34f71029182b409d6d7b
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-14 16:01:55 +0200

    Update getParents.js

--- core/methods/getParents.js
+++ core/methods/getParents.js
@@ -1,11 +1,12 @@
 // getParents.js
 
-import { resolveElement } from './resolveElement.js';
 import { buildSelector }  from './buildSelector.js';
+import { resolveElement } from './resolveElement.js';
 import { walk }           from './../utils.js';
 
-const passes = (element, filter) => !filter || element.matches(buildSelector(filter));
-
 // Ascending up to the root, closest ancestor first
-export function getParents (spec, filter) { return walk(resolveElement(spec), 'parentElement', filter, true); }
+export function getParents (spec, filter) {
+  return walk(resolveElement(spec), 'parentElement', filter, true);
+}
+
 export default getParents;
