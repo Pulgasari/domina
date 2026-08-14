b7c5174f8d9ad9cf53b935984f5b310a07756dc5
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-08-14 16:04:41 +0200

    Update getPrevAll.js

--- core/methods/getPrevAll.js
+++ core/methods/getPrevAll.js
@@ -1,10 +1,11 @@
 // getPrevAll.js
 
-import { resolveElement } from './resolveElement.js';
 import { buildSelector }  from './buildSelector.js';
+import { resolveElement } from './resolveElement.js';
 import { walk }           from './../utils.js';
 
-const passes = (element, filter) => !filter || element.matches(buildSelector(filter));
+export function getPrevAll (spec, filter) {
+  return walk (resolveElement(spec), 'previousElementSibling', filter, true);
+}
 
-export function getPrevAll (spec, filter) { return walk(resolveElement(spec), 'previousElementSibling', filter, true); }
 export default getPrevAll;
