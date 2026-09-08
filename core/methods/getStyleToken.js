69fad7f563baa5ed0bb9dede1bc2c06cb94f1573
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-08 14:36:07 +0200

    Update getStyleToken.js

--- core/methods/getStyleToken.js
+++ core/methods/getStyleToken.js
@@ -2,7 +2,7 @@
 
 import getComputedStyle from './getComputedStyle.js';
      
-const ensurePrefix = (value, prefix) => value.startsWith(prefix) ? value : (prefix + name);     
+const ensurePrefix = (value, prefix) => value.startsWith(prefix) ? value : (prefix + value);     
 
 export function getStyleToken (property, spec, inline = false) {
   spec ||= document.documentElement;
@@ -10,10 +10,6 @@ export function getStyleToken (property, spec, inline = false) {
   const prop  = ensurePrefix(property, '--');
   const style = getComputedStyle(spec);
   const token = style?.getPropertyValue(prop).trim() || null;
-
-  console.log('property:', property);
-  console.log('prop:', prop);
-  console.log('token:', token);
 
   return token;
 }
